aa81512a5a4e1bafc8325e8d5529b4eecdaf3936
morozov1982 <morozov1982@gmail.com>
2022-04-19 21:33:10 +0600

    add: lesson_04 files

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'продукты'
+
+    products = Product.objects.all()  # [:2]
+    categories = ProductCategory.objects.all()
+
+    context = {
+        'title': title,
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'mainapp/products.html', context)
