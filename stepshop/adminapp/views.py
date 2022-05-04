ae8579dd633d8b36bb04ea51d7b4c698737172b8
morozov1982 <morozov1982@gmail.com>
2022-05-04 19:33:07 +0600

    add: lesson_11 start

---
+++ stepshop/adminapp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+from authapp.models import ShopUser
+from mainapp.models import Product, ProductCategory
+
+
+def user_create(request):
+    pass
+
+
+def users(request):
+    pass
+
+
+def user_update(request):
+    pass
+
+
+def user_delete(request):
+    pass
+
+
+def category_create(request):
+    pass
+
+
+def categories(request):
+    pass
+
+
+def category_update(request):
+    pass
+
+
+def category_delete(request):
+    pass
+
+
+def product_create(request):
+    pass
+
+
+def products(request):
+    pass
+
+
+def product_read(request):
+    pass
+
+
+def product_update(request):
+    pass
+
+
+def product_delete(request):
+    pass
+
+
